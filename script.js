05549554e9cf9c1bd19f646a2c8931399353bf06
Chris Benjamin <cbenjamin2009@gmail.com>
2025-05-02 09:09:16 -0700

    redesign to 2 data points

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ fetch('RDC.csv')
     .then(response => response.text())
     .then(csvData => {
         const closingDates = parseCSV(csvData, 'closingdate');
+        const mutualDates = parseCSV(csvData, 'mutualdate');
         const latestDate = getLatestDate(closingDates);
-        const daysSince = daysSinceLastDate(latestDate);
-        renderDaysSince(daysSince);
+        const latestMutualDate = getLatestDate(mutualDates);
+        const daysSinceLastClose = daysSinceLastDate(latestDate);
+        const daysSinceMutual = daysSinceLastDate(latestMutualDate)
+        renderDaysSince(daysSinceLastClose, daysSinceMutual);
     })
     .catch(error => console.error('Error fetching the CSV file:', error));
 
@@ -31,41 +34,14 @@ function daysSinceLastDate(latestDate) {
     return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 }
 
-function renderDaysSince(daysSince) {
-    const daysElement = document.getElementById('days');
-    daysElement.innerHTML = daysSince;
-    updateArc(daysSince);
+function renderDaysSince(daysSinceLastClose, daysSinceMutual) {
+    const daysLastCloseElement = document.getElementById('lastclose');
+    const daysMutualElement = document.getElementById('lastmutual');
+    daysLastCloseElement.innerHTML = daysSinceLastClose;
+    daysMutualElement.innerHTML = daysSinceMutual;
+
 }
 
-const width = 300;
-const height = 300;
-const radius = Math.min(width, height) / 2;
-const svg = d3.select("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .append("g")
-    .attr("transform", `translate(${width / 2}, ${height / 2})`);
-
-const arc = d3.arc()
-    .innerRadius(radius - 50)
-    .outerRadius(radius - 20)
-    .startAngle(0);
-
-const background = svg.append("path")
-    .datum({ endAngle: 2 * Math.PI })
-    .style("fill", "#ddd")
-    .attr("d", arc);
-
-const foreground = svg.append("path")
-    .datum({ endAngle: 0 })
-    .attr("class", "arc")
-    .attr("d", arc);
-
-function updateArc(days) {
-    console.log('Updated arc to display', days, 'days');
-    const endAngle = (days / 365) * 2 * Math.PI;
-    foreground.datum({ endAngle: endAngle }).attr("d", arc);
-}
 
 function autoRefresh() {
     const now = new Date();
